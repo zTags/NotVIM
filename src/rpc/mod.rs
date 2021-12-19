e721424cf43347230f2511355b82c2f8451ccfda
Deltara3 <50772027+Deltara3@users.noreply.github.com>
2021-12-19 18:06:15 -0500

    removed thing i needed to remove

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,7 +1,7 @@
 use discord_rich_presence::{activity, new_client, DiscordIpc};
 
 pub fn start_rpc() -> Result<(), Box<dyn std::error::Error>> {
-    let client_id = "922164198715301919";
+    let client_id = "id";
     let mut client = new_client(client_id)?;
     client.connect()?;
     client.set_activity(activity::Activity::new()
