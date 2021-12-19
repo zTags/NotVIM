4fef3731a62d51667830bb46694ce4664af7a137
Deltara3 <50772027+Deltara3@users.noreply.github.com>
2021-12-19 18:44:42 -0500

    basic rpc

--- src/rpc/mod.rs
+++ src/rpc/mod.rs
@@ -1,14 +1,27 @@
 use discord_rich_presence::{activity, new_client, DiscordIpc};
+use std::thread;
 
-pub fn start_rpc() -> Result<(), Box<dyn std::error::Error>> {
-    let client_id = "id";
-    let mut client = new_client(client_id)?;
-    client.connect()?;
-    client.set_activity(activity::Activity::new()
-        .state("test")
-        .details("test")
-    )?;
-    client.close()?;
+pub fn start_rpc() {
+    thread::spawn(|| {
+        let version: &str = env!("CARGO_PKG_VERSION");
+        let client_id = "client id";
+        let mut client = new_client(client_id).unwrap();
+        client.connect().unwrap();
+        client.set_activity(activity::Activity::new()
+            .state("Not editing")
+            .details("Idle")
+            .assets(activity::Assets::new()
+                .large_image("idle")
+                .large_text("Idle")
+                .small_image("logo")
+                .small_text(&format!("NotVIM v{}", version))
+            )
+        ).unwrap();
 
-    Ok(())
+        loop {}
+    });
+}
+
+pub fn stop_rpc() {
+    // TODO implement
 }
